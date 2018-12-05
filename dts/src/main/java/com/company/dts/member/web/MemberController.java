2c05d1a5beb834d22381325c40737a310faae6f5
seosangchan02@gmail.com <seosangchan02@gmail.com>
2018-12-05 14:55:04 +0900

    home.jsp membercontroller.java membermapper.xml 수정 로그인 댐

--- dts/src/main/java/com/company/dts/member/web/MemberController.java
+++ dts/src/main/java/com/company/dts/member/web/MemberController.java
@@ -1,8 +1,11 @@
 package com.company.dts.member.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -66,4 +69,25 @@ public class MemberController {
 		memberService.deleteMemberList(vo);	//여러개 삭제처리
 		return "redirect:getMemberList";		//목록요청
 	}*/
+	
+	@RequestMapping("login")   // 
+	public String login(@ModelAttribute("member") MemberVO vo, HttpSession session) {
+		// id 단건조회
+		MemberVO membervo = memberService.getMember(vo);
+		// id가 있으면 패스워드 비교
+		if(membervo == null) { // id 없으면
+			return "home";
+		} else if (! vo.getuPw().equals(membervo.getuPw())) { // ! <- not
+			return "home";
+		} else {
+			session.setAttribute("login", membervo);
+			return "redirect:getPurchaseList";
+		}
+	}
+	// 로그아웃
+	@RequestMapping("logout")
+	public String logout(HttpSession session) {
+		session.invalidate(); // 세션 무효화 (로그아웃)
+		return "home";
+	}
 }
