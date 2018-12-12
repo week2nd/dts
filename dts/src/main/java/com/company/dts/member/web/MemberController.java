3356998d6bfb85fb4f840bbfaa76bfdac49780ff
seosangchan02@gmail.com <seosangchan02@gmail.com>
2018-12-12 10:52:03 +0900

    usermain.jsp membercontroller.java tiles.xml

--- dts/src/main/java/com/company/dts/member/web/MemberController.java
+++ dts/src/main/java/com/company/dts/member/web/MemberController.java
@@ -98,19 +98,19 @@ public class MemberController {
 		MemberVO membervo = memberService.getMember(vo);
 		// id가 있으면 패스워드 비교
 		if(membervo == null) { // id 없으면
-			return "home";
+			return "/";
 		} else if (! vo.getuPw().equals(membervo.getuPw())) { // ! <- not
-			return "home";
+			return "/";
 		} else {
 			session.setAttribute("membersession", membervo);
-			return "redirect:getGameList";
+			return "user/main/userMain";
 		}
 	}
 	// 로그아웃
 	@RequestMapping("logout")
 	public String logout(HttpSession session) {
 		session.invalidate(); // 세션 무효화 (로그아웃)
-		return "homeUser";
+		return "/";
 	}
 	
 	
